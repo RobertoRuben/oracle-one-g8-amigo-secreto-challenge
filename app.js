fca0fe005b61ca8e07d7d371a038ba7445205ff6
RobertoRuben <robertoch263@gmail.com>
2025-02-02 16:54:53 -0500

    ✨ feat: implement add friend function to manage friend list

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let friendList = [];
+
+function addFriend() {
+    let friendNameInput = document.getElementById('amigo');
+    let friendName = friendNameInput.value;
+
+    if (friendName === ''){
+        alert('El nombre del amigo no puede estar vacío');
+        return;
+    }
+    friendList.push(friendName);
+    friendNameInput.value = '';
+}
